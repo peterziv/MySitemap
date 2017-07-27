24fe1c3c1e165c7834d297196995d95b5aa1ba4a
peterziv <peter.ziv@hotmail.com>
2017-07-28 01:51:39 +0800

    Add some help information

--- src/default.php
+++ src/default.php
@@ -5,6 +5,11 @@
 require_once __DIR__ . '/Sitemap.php';
 
 error_reporting(E_ERROR);
+define('MYSITEMAP_VERSION', '1.0.0');
+
+echo 'MySitemap v' . MYSITEMAP_VERSION . PHP_EOL;
+echo 'Copyright (C) 2017 PETER with LGPL-2.1 License' . PHP_EOL;
+echo PHP_EOL;
 
 $kd = false;
 switch ($argc) {
@@ -17,7 +22,10 @@ switch ($argc) {
         $url = $argv[1];
         break;
     default:
-        echo '[INFO] syntax error! Please use php mysitemap-xxxx.phar domain [--kd]';
+        echo '[INFO] syntax error! Please use php mysitemap-xxxx.phar domain [--kd]' . PHP_EOL;
+        echo PHP_EOL;
+        echo 'Usage Command: php mysitemap-' . MYSITEMAP_VERSION . '.phar domain [--kd]' . PHP_EOL;
+        echo '[--kd] if you want to get keywords and description in sitemap, plesae add this parameter' . PHP_EOL;
         exit(-1);
 }
 $site = new \ZKit\seo\Sitemap();
